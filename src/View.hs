57cc9b444f8242bdb34d6d83196445238f898604
Tony Hosking <antony.hosking@anu.edu.au>
2020-07-29 16:22:55 +1000

    First commit

---
+++ src/View.hs
@@ -0,0 +1,8 @@
+--- Copyright 2020 The Australian National University, All rights reserved
+module View where
+
+import CodeWorld
+
+-- this function can be assigned different pictures
+myPicture :: Picture
+myPicture = blank
